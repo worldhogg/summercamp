1d23730edea6bc7c74f688af5815d9be6f70728d
worldhogg <art_shidichev07@mail.ru>
2026-06-12 19:17:18 +0500

    learnind day 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
+#include <unordered_map>
+#include <vector>
+vector<int> twosum(vector<int>& vec, int target ){
+    unordered_map<int,int>mymap;
+    for(int i = 0; i < vec.size();i++){
+        int need = target-vec[i];
+        if (mymap.find(need) != mymap.end()){
+            return {mymap[need],i};
+        }
+        else{
+            mymap[vec[i]] = i;
+        }
+    }
+    return{};
+
+    }
 int main(){
-<<<<<<< HEAD
-    cout << "hello from main";
-=======
-    cout << "hello from feature";
->>>>>>> feat
+    int n;
+    cin >> n;
+    vector <int> vec(n);
+    for (int i = 0; i < vec.size();i++){
+        cin >> vec[i];
+    }
+    int target;
+    cin >> target;
+    vector<int> ans = twosum(vec,target);
+    cout << ans[0] <<" "<< ans[1];
 }
